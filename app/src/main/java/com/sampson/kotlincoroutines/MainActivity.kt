9697715cc399fa9ae7393f933868407b0696a07b
fsampson30@ <fsampson30@gmail.com>
2021-12-08 17:09:19 -0300

    Fixed Gradle issues.

--- app/src/main/java/com/sampson/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/sampson/kotlincoroutines/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sampson.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edtBaseText = findViewById<EditText>(R.id.edtMainActivityBaseText)
+        val btnStart = findViewById<Button>(R.id.btnMainActivityStartCoroutine)
+        val txtShowResult = findViewById<TextView>(R.id.txtMainActivityShowResult)
+        val txtShowTimesRepeated = findViewById<TextView>(R.id.txtMainActivityShowTimesRepeated)
     }
 }
