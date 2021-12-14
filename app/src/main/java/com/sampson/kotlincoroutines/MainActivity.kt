93587268eef3063263a25a23ec04079fb28e02cb
fsampson30@ <fsampson30@gmail.com>
2021-12-14 08:29:47 -0300

    Created logic from a google codelab to practice.

--- app/src/main/java/com/sampson/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/sampson/kotlincoroutines/MainActivity.kt
@@ -2,10 +2,7 @@ package com.sampson.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btnMainActivityStartCoroutine)
         val txtShowResult = findViewById<TextView>(R.id.txtMainActivityShowResult)
         val txtShowTimesRepeated = findViewById<TextView>(R.id.txtMainActivityShowTimesRepeated)
+        val btnRollDice = findViewById<Button>(R.id.btnMainActivityRollDice)
+        val txtNumerRolls = findViewById<TextView>(R.id.txtMainActivityNumberRolls)
+        val imgView = findViewById<ImageView>(R.id.imgViewMainActivityDice)
 
         btnStart.setOnClickListener {
             txtShowResult.text = edtBaseText.text
@@ -36,6 +36,13 @@ class MainActivity : AppCompatActivity() {
             }
             Toast.makeText(this, "Finish",Toast.LENGTH_SHORT).show()
         }
+
+        btnRollDice.setOnClickListener {
+            rollDice()
+            val number = randomDiceNumber()
+            txtNumerRolls.text = number
+            imgView.setImageResource(drawableResourse(number.toInt()))
+        }
     }
 
     private suspend fun doTimesRepeated(): String {
@@ -46,6 +53,28 @@ class MainActivity : AppCompatActivity() {
     private suspend fun showMessage(): String {
         delay(3000)
         return "Returning"
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun randomDiceNumber(): String {
+        return (1..6).random().toString()
+    }
+
+    private fun drawableResourse(param : Int) : Int {
+        val diceImage : Int
+        when (param) {
+            1 -> diceImage = R.drawable.dice_1
+            2 -> diceImage = R.drawable.dice_2
+            3 -> diceImage = R.drawable.dice_3
+            4 -> diceImage = R.drawable.dice_4
+            5 -> diceImage = R.drawable.dice_5
+            6 -> diceImage = R.drawable.dice_6
+            else -> diceImage = R.drawable.empty_dice
+        }
+        return diceImage
     }
 
 }
