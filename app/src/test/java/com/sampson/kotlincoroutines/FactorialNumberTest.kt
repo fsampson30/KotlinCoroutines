1f3cc8271b9baff5fc6b7dc6b00a6b7caf1cacbb
fsampson30@ <fsampson30@gmail.com>
2021-12-27 10:13:02 -0300

    Parameterized test created.

--- app/src/test/java/com/sampson/kotlincoroutines/FactorialNumberTest.kt
+++ app/src/test/java/com/sampson/kotlincoroutines/FactorialNumberTest.kt
@@ -3,16 +3,32 @@ package com.sampson.kotlincoroutines
 import org.junit.Test
 import com.google.common.truth.Truth.assertThat
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import org.junit.runners.Parameterized
 
-@RunWith(JUnit4::class)
-class FactorialNumberTest {
+@RunWith(Parameterized::class)
+class FactorialNumberTest(
+    private val input: Int,
+    private val expected: Int
+) {
+
+    companion object {
+        @Parameterized.Parameters
+        @JvmStatic
+        fun getData(): Collection<Array<Int>> = listOf(
+            arrayOf(0, 1),
+            arrayOf(1, 1),
+            arrayOf(2, 2),
+            arrayOf(3, 6),
+            arrayOf(4, 24),
+            arrayOf(5, 120)
+        )
+    }
+
     private val factorial = FactorialNumber()
 
     @Test
     fun computesFactorial() {
-        val n = 3
-        val result = factorial.factorial(n)
-        assertThat(result).isEqualTo(6)
+        val result = factorial.factorial(input)
+        assertThat(result).isEqualTo(expected)
     }
 }
