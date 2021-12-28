c0bd8bb0910f237f904afe0646a4ac4e811cd03b
fsampson30@ <fsampson30@gmail.com>
2021-12-28 08:06:28 -0300

    Removed IntArray and used For Loop Instead.

--- app/src/main/java/com/sampson/kotlincoroutines/FactorialNumber.kt
+++ app/src/main/java/com/sampson/kotlincoroutines/FactorialNumber.kt
@@ -1,18 +1,26 @@
 package com.sampson.kotlincoroutines
 
+import java.math.BigInteger
 import kotlin.jvm.Throws
 
 class FactorialNumber {
     @Throws(FactorialNotFoundException::class)
-    fun factorial(n: Int): Int {
+    fun factorial(n: Int): BigInteger {
         if (n < 0) {
             throw FactorialNotFoundException
         }
-        return IntArray(n) {
+        // Refactored to eliminate IntArray and usage of Memory, created a For Loop Instead.
+        /*return IntArray(n) {
             it + 1
-        }.fold( 1, { acc, i ->
-            acc * i
-        } )
+        }.fold( BigInteger.ONE, { acc, i ->
+            acc * i.toBigInteger()
+        } )*/
+        var result = BigInteger.ONE
+        for (i in 1..n){
+            result = result.times(i.toBigInteger())
+        }
+        return result
     }
+
     object FactorialNotFoundException : Throwable()
 }
--- app/src/test/java/com/sampson/kotlincoroutines/FactorialNumberTest.kt
+++ app/src/test/java/com/sampson/kotlincoroutines/FactorialNumberTest.kt
@@ -4,23 +4,26 @@ import org.junit.Test
 import com.google.common.truth.Truth.assertThat
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
+import java.math.BigInteger
 
 @RunWith(Parameterized::class)
 class FactorialNumberTest(
     private val input: Int,
-    private val expected: Int
+    private val expected: BigInteger
 ) {
 
     companion object {
         @Parameterized.Parameters
         @JvmStatic
-        fun getData(): Collection<Array<Int>> = listOf(
-            arrayOf(0, 1),
-            arrayOf(1, 1),
-            arrayOf(2, 2),
-            arrayOf(3, 6),
-            arrayOf(4, 24),
-            arrayOf(5, 120)
+        fun getData(): Collection<Array<Any>> = listOf(
+            arrayOf(0, BigInteger.ONE),
+            arrayOf(1, BigInteger.ONE),
+            arrayOf(2, BigInteger.valueOf(2)),
+            arrayOf(3, BigInteger.valueOf(6)),
+            arrayOf(4, BigInteger.valueOf(24)),
+            arrayOf(5, BigInteger.valueOf(120)),
+            arrayOf(13, BigInteger("6227020800")),
+            arrayOf(25, BigInteger("15511210043330985984000000"))
         )
     }
 
